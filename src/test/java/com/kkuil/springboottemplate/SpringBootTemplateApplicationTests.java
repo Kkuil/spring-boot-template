0410155f971800197093db7e78410773707bdba0
Kkuil <3024067144@qq.com>
2023-09-02 19:50:52 +0800

    fixed:jasypt-version -> 3.0.5

--- src/test/java/com/kkuil/springboottemplate/SpringBootTemplateApplicationTests.java
+++ src/test/java/com/kkuil/springboottemplate/SpringBootTemplateApplicationTests.java
@@ -1,13 +1,23 @@
 package com.kkuil.springboottemplate;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootTemplateApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private StringEncryptor stringEncryptor;
 
+    @Test
+    void testJasypt() {
+        String username = "username";
+        String password = "password";
+        String encryptUsername = stringEncryptor.encrypt(username);
+        String encryptPassword = stringEncryptor.encrypt(password);
+        System.out.println("encryptUsername = " + encryptUsername);
+        System.out.println("encryptPassword = " + encryptPassword);
+    }
 }
