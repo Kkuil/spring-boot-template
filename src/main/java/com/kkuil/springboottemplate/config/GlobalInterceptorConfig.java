74ead76fff7749b22a347b679bbc90ec09c149fd
Kkuil <3024067144@qq.com>
2023-09-04 11:07:47 +0800

    add: LogInterceptor

--- src/main/java/com/kkuil/springboottemplate/config/GlobalInterceptorConfig.java
+++ src/main/java/com/kkuil/springboottemplate/config/GlobalInterceptorConfig.java
@@ -1,14 +1,33 @@
 package com.kkuil.springboottemplate.config;
 
-import com.kkuil.springboottemplate.interceptors.LoginInterceptor;
+import com.kkuil.springboottemplate.interceptors.LogInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * @Author Kkuil
+ * @Description 全局拦截器配置
+ * @Date 2023/09/03
+ */
 @Configuration
 public class GlobalInterceptorConfig implements WebMvcConfigurer {
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**");
+    public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+        interceptorRegistry.addInterceptor(new LogInterceptor());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                // 允许发送 Cookie
+                .allowCredentials(true)
+                // 放行哪些域名（必须用 patterns，否则 * 会和 allowCredentials 冲突）
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+
     }
 }
